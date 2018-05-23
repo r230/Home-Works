cde26e91d6650792aa791b856e12da8235a5479f
Ren <r230713@gmail.com>
2018-05-23 23:18:50 +0300

    Update hw_2.py

--- hw_2.py
+++ hw_2.py
@@ -5,14 +5,26 @@ print(date[3:6] + date[0:3] + date[6:])
 name = 'Mark Zuckerberg'
 print(name[-10:] + name[-11] + name[-15:-11])
 
-name_2 = 'Mark Zuckerberg'
-name_2_lst = name_2.split(' ')
-name_2_lst = name_2_lst[::-1]
-reverse = ' '.join(name_2_lst)
-print(reverse)
 
 snake_case = 'employee_first_name'
 camel_case = snake_case.split('_')
 print(camel_case[0].capitalize() + camel_case[1].capitalize() + camel_case[2].capitalize())
 
 
+user_data = 'Marcus Aurelius*121-04-26*180-03-17'
+list = user_data.split('*')
+first_date = list[1]
+second_date = list[2]
+born_year = first_date.split('-')[0]
+death_year = second_date.split('-')[0]
+age = int(death_year) - int(born_year)
+print(list[0] + ',', age)
+
+user_data = 'Leo Tolstoy*1828-08-28*1910-11-20'
+list = user_data.split('*')
+first_date = list[1]
+second_date = list[2]
+born_year = first_date.split('-')[0]
+death_year = second_date.split('-')[0]
+age = int(death_year) - int(born_year)
+print(list[0] + ',', age)
