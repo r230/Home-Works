328f98cf0917a08501173e034aae46f0e604c9cf
Ren <r230713@gmail.com>
2018-05-23 14:00:03 +0300

    Add files via upload

---
+++ hw_2.py
@@ -0,0 +1,13 @@
+date = '05.17.2016'
+print(date[3:6] + date[0:3] + date[6:])
+
+
+name = 'Mark Zuckerberg'
+print(name[-10:] + name[-11] + name[-15:-11])
+
+
+snake_case = 'employee_first_name'
+camel_case = snake_case.split('_')
+print(camel_case[0].capitalize() + camel_case[1].capitalize() + camel_case[2].capitalize())
+
+
